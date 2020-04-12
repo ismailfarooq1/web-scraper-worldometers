95b710c766c966170275f45fbeec43569013e9ca
Ismail Farooq <ismaeelfarooq53@gmail.com>
2020-04-12 19:27:49 +0500

    changing the User-agent.

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -6,7 +6,13 @@ import logging
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info']
-    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+    start_urls = [
+        'https://www.worldometers.info/world-population/population-by-country']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.worldometers.info/world-population/population-by-country', callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0'
+        })
 
     def parse(self, response):
         countries = response.xpath("//td/a")
@@ -17,17 +23,22 @@ class CountriesSpider(scrapy.Spider):
             # absolute_url = f"https://www.worldometers.info{link}"
             # absolute_url = response.urljoin(link)
 
-            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name})
-        
+            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name}, headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0'
+            })
+
     def parse_country(self, response):
         name = response.request.meta['country_name']
-        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        rows = response.xpath(
+            "(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
         for row in rows:
-             years = row.xpath('.//td[position() = 1]/text()').get()
-             population = row.xpath('.//td[position() = 2]/strong/text() ').get()
+            years = row.xpath('.//td[position() = 1]/text()').get()
+            population = row.xpath(
+                './/td[position() = 2]/strong/text() ').get()
 
-             yield {
-                 'country_name': name,
-                 'years': years,
-                 'population': population
-             }
+            yield {
+                'country_name': name,
+                'years': years,
+                'population': population,
+                'User-Agent': response.request.headers['User-Agent']
+            }
